d384ab3bd4573ddc35e918d12c0f15100dd68c63
Jonasz Karwacki <jonaszakr@gmail.com>
2020-04-18 11:47:10 -0700

    Complete minimum reqs per assignment

--- index.js
+++ index.js
@@ -1,12 +1,30 @@
-addEventListener('fetch', event => {
-  event.respondWith(handleRequest(event.request))
-})
 /**
- * Respond with hello worker text
- * @param {Request} request
- */
-async function handleRequest(request) {
-  return new Response('Hello worker!', {
-    headers: { 'content-type': 'text/plain' },
-  })
+* Get a random element in array
+* @param {object} array
+*/
+function randElm(array) {
+    if (array.length > 0) {
+        return array[Math.floor(Math.random() * array.length)]
+    }
 }
+
+/**
+* Respond with random variance response
+* @param {Request} request
+*/
+async function handleRequest(request) {
+    const variantsUrl = 'https://cfw-takehome.developers.workers.dev/api/variants'
+
+    // Store a random variant from variantsUrl response
+    let randVariant = await fetch(variantsUrl)
+        .then(response => response.json())
+        .then(json => json['variants'])
+        .then(variants => randElm(variants))
+
+    // Give back variant response
+    return fetch(randVariant)
+}
+
+addEventListener('fetch', event => {
+    event.respondWith(handleRequest(event.request))
+})
