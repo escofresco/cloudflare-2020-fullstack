85bf47c632d7483b64bae8d747767a7ab79c9881
Jonasz Karwacki <jonaszakr@gmail.com>
2020-04-22 09:10:46 -0700

    fix missed cookie issue

--- index.js
+++ index.js
@@ -1,6 +1,5 @@
 /**
 * Get a random element in array
-* @param {object} array
 */
 function randElm(array) {
     if (array.length > 0) {
@@ -9,20 +8,75 @@ function randElm(array) {
 }
 
 /**
-* Respond with random variance response
+* Get a random key,value from a Map as an object
+* @param {Map} map
+*/
+function randKV(map) {
+    let randKey = randElm(Array.from(map.keys()))
+    return { 'key': randKey, 'value': map.get(randKey) }
+}
+
+/**
+* Classify a cloudflare variant url into a group based on the final
+* item in route path.
+* @param {String} variant
+*/
+function variantGroup(variant) {
+    let urlParts = variant.split('/')
+    return urlParts[urlParts.length - 1]
+}
+
+/**
+* Classify each variant url into a group and then return that as a map
+* where groups are keys and urls are values
+* @param {Object} variantArray
+*/
+function asVariantMap(variantArray) {
+    let variantMap = new Map()
+    variantArray.forEach(variantUrl => {
+        variantMap.set(variantGroup(variantUrl), variantUrl)
+    })
+    return variantMap
+}
+
+/**
+* Respond with random variant response
 * @param {Request} request
 */
 async function handleRequest(request) {
-    const variantsUrl = 'https://cfw-takehome.developers.workers.dev/api/variants'
 
-    // Store a random variant from variantsUrl response
-    const randVariant = await fetch(variantsUrl)
+    const VARIANTS_URL = 'https://cfw-takehome.developers.workers.dev/api/variants'
+    const COOKIE_NAME = 'Variant'
+    const cookie = request.headers.get('cookie')
+
+    // Map the variant group to its url
+    let variantMap = await fetch(VARIANTS_URL)
         .then(response => response.json())
-        .then(json => json['variants'])
-        .then(variants => randElm(variants))
+        .then(json => asVariantMap(json['variants']))
+    let variantKeys = Array.from(variantMap)
+    let response
 
-    // Give back variant response
-    return fetch(randVariant)
+    variantMap.forEach(async (variantUrl, variantGroup, _) => {
+        //~~> Go through the variantMap by key and see if a cookie for one of
+        // the items exists
+        if (cookie && cookie.includes(`${COOKIE_NAME}=${variantGroup}`))
+            // We found a cookie for one of the variant urls
+            response = fetch(variantUrl)
+    })
+
+    if (response === undefined) {
+        //~~> Cookie for a variant must not exist, so make a new Response
+        // from a randomly picked variant
+        let randVariantKV = randKV(variantMap)
+        let randVariantGroup = randVariantKV.key
+        let randVariantUrl = randVariantKV.value
+        response = await fetch(randVariantUrl)
+            .then(response => new Response(response.body))
+        response.headers.append('Set-Cookie', `${COOKIE_NAME}=${randVariantGroup}`)
+        return response
+    } else {
+        return await response
+    }
 }
 
 addEventListener('fetch', event => {
